9c4208cf6ce0d8d25c98ad34936d98f26658bd7c
Jasper <>
2022-11-25 02:37:48 +0800

    update

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -4,7 +4,7 @@ import BasicsPage from 'flarum/admin/components/BasicsPage';
 app.initializers.add('capomod-home-page', () => {
   extend(BasicsPage.prototype, 'homePageItems', (items) => {
     items.add('capomod', {
-      path: 'http://capomod.com/blog/category/recommend',
+      path: '/blog',
       label: 'CapoModHomePage',
     });
   });
